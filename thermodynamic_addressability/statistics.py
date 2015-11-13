7f4ae1f4da368d9cd6e64f18f7f477aa4ddee21c
Jerzy Kozyra <j.w.kozyra@ncl.ac.uk>
2015-11-13 17:20:10 +0000

    Sequences and verification

---
+++ thermodynamic_addressability/statistics.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def calc_levenshtein_dist(source, target):
+    if len(source) < len(target):
+        return levenshtein(target, source)
+    # So now we have len(source) >= len(target).
+    if len(target) == 0:
+        return len(source)
+    # We call tuple() to force strings to be used as sequences
+    # ('c', 'a', 't', 's') - numpy uses them as values by default.
+    source = np.array(tuple(source))
+    target = np.array(tuple(target))
+    # We use a dynamic programming algorithm, but with the
+    # added optimization that we only need the last two rows
+    # of the matrix.
+    previous_row = np.arange(target.size + 1)
+    for s in source:
+        # Insertion (target grows longer than source):
+        current_row = previous_row + 1
+        # Substitution or matching:
+        # Target and source items are aligned, and either
+        # are different (cost of 1), or are the same (cost of 0).
+        current_row[1:] = np.minimum(
+                current_row[1:],
+                np.add(previous_row[:-1], target != s))
+        # Deletion (target grows shorter than source):
+        current_row[1:] = np.minimum(
+                current_row[1:],
+                current_row[0:-1] + 1)
+        previous_row = current_row
+    return previous_row[-1]
+
+
+print calc_levenshtein_dist('hello how you doing?', 'hi how are you?')
+print calc_levenshtein_dist('dont', 'fuck')
+print calc_levenshtein_dist('it', 'up')
+
